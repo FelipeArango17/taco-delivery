c24503389c813973dbdb5dce456ac1060d5db239
Felezzz <arangofelipe2007@gmail.com>
2025-10-20 16:32:22 -0500

    Se agrego index y detalles minimos

---
+++ src/main/java/com/tacodelivery/tacodelivery/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.tacodelivery.tacodelivery.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/index"})
+    public String mostrarIndex() {
+        return "index"; // Renderiza templates/index.html
+    }
+}
